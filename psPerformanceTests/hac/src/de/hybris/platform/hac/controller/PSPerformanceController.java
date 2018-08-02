c5996d539d4cba20e7d6660501bcf0c9061ea216
Cieslo <41468689+Cieslo@users.noreply.github.com>
2018-08-02 10:44:20 +0200

    improt Logger

--- psPerformanceTests/hac/src/de/hybris/platform/hac/controller/PSPerformanceController.java
+++ psPerformanceTests/hac/src/de/hybris/platform/hac/controller/PSPerformanceController.java
@@ -24,6 +24,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.apache.log4j.Logger;
+
 
 /**
  *
